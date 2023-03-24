b5a989437dfb5bb1d368a4e569cf31094066f981
Simon Pletersek <pletersek.si@gmail.com>
2023-03-25 00:19:18 +0100

    final routing

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -28,4 +28,10 @@ export class RecipeDetailComponent implements OnInit {
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
+
+  onEditRecipe() {
+    // this.router.navigate([`../${this.id}/edit`], { relativeTo: this.route });
+    this.router.navigate(['edit'], { relativeTo: this.route });
+    // console.log(this.route.data);
+  }
 }
